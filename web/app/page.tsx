8b07ccd314ef4b3b6918cf8d1fd77cf452670f62
Cristiano Junior <cristianojunior120@gmail.com>
2023-05-16 23:21:31 -0300

    criado a estrutura do front-end do sistema

---
+++ web/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+          <div className="h-screen bg-zinc-950">
+            <h1 className="text-zinc-50 text-4xl font-bold p-6">Sua Cápsula do tempo</h1>
+          </div>
+    )
+}
