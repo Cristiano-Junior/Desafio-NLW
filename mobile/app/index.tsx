3b2fc0e9bf8bb1979dbdaf858a0541c04e08ed1f
Cristiano Junior <cristianojunior120@gmail.com>
2023-05-23 23:45:35 -0300

    concluído autenticação com o GitHub, na parte da web foi salvo o token em um Cookie, já no mobile foi salvo no Secure Store do Expo, mudança no layout ao logar, mobile agora com expo router

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,16 +1,29 @@
+import { useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar'
 import { ImageBackground, Text, View, TouchableOpacity } from 'react-native'
 import { styled } from 'nativewind'
+import { api } from '../src/lib/api'
+import * as SecureStore from 'expo-secure-store';
+import { useRouter } from "expo-router";
 
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
 
-import blurBg from './src/assets/bg-blur.png'
-import Stripes from './src/assets/stripes.svg'
-import NLWlogo from './src/assets/nlw-spacetime-logo.svg'
+import blurBg from '../src/assets/bg-blur.png'
+import Stripes from '../src/assets/stripes.svg'
+import NLWlogo from '../src/assets/nlw-spacetime-logo.svg'
+import { useAuthRequest, makeRedirectUri } from 'expo-auth-session'
+
+const discovery = {
+  authorizationEndpoint: 'https://github.com/login/oauth/authorize',
+  tokenEndpoint: 'https://github.com/login/oauth/access_token',
+  revocationEndpoint: 'https://github.com/settings/connections/applications/6cce7529d0c46f851bb3',
+};
 
 
 export default function App() {
+
+  const router = useRouter();
 
   const [hasLoadedFonts] = useFonts({
     Roboto_400Regular,
@@ -18,7 +31,46 @@ export default function App() {
     BaiJamjuree_700Bold
   })
 
+  const [, response, signInWithGithub] = useAuthRequest(
+    {
+      clientId: '6cce7529d0c46f851bb3',
+      scopes: ['identity'],
+      redirectUri: makeRedirectUri({
+        scheme: 'nlwspacetime'
+      }),
+    },
+    discovery
+  );
+
   const Styledstripes = styled(Stripes)
+
+  async function handleGithubOAuthCode(code: string) {
+
+    const response = await api.post('/register', {
+        code,
+      })
+
+      const { token } = response.data
+        
+      await SecureStore.setItemAsync('token', token)
+
+      router.push('memories')
+  }
+
+
+  useEffect(() => {
+
+    // console.log(makeRedirectUri({
+    //   scheme: 'nlwspacetime'
+    // }))
+  
+    if (response?.type === 'success') {
+      const { code } = response.params
+
+      handleGithubOAuthCode(code)
+
+    }
+  }, [response]);
 
   if (!hasLoadedFonts) {
     return null
@@ -47,6 +99,7 @@ export default function App() {
         <TouchableOpacity 
           activeOpacity={0.7}
           className='rounded-full bg-green-500 px-5 py-3'
+          onPress={() => signInWithGithub()}
         >
           <Text className='font-alt text-sm uppercase text-black'>Cadastrar Lembranças</Text>
 
